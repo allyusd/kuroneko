4b5caeef5740e9409537175d6f0a0f141cd99cec
allyusd <allyusd@yahoo.com.tw>
2014-07-17 04:26:56 +0800

    replace 'ui.router' with 'angular-ui-router' for Nested States & Views

--- js/app.js
+++ js/app.js
@@ -2,15 +2,32 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
-  'ngRoute',
+  'ui.router',
   'myApp.controllers',
   'myApp.services',
   'myApp.directives'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to
+  $urlRouterProvider.otherwise("/main");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('main', {
+      url: "/main",
+      templateUrl: "partials/main.html"
+    })
+    .state('main.view1', {
+      url: "/view1",
+      templateUrl: "partials/partial1.html",
+      controller: "MyCtrl1"
+    })
+    .state('main.view2', {
+      url: "/view2",
+      templateUrl: "partials/partial2.html",
+      controller: "MyCtrl2"
+    });
 }]).
 run(['gamecore', function(gamecore) {
 	console.log(gamecore.sayHello());
